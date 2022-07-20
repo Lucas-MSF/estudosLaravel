2f91e2bbc08a617db51d79ddf7cf682ca6da86fc
Lucas-MSF <lucasmasafi@gmail.com>
2022-07-20 09:33:53 -0300

    .

---
+++ padroesMVC/View/Cursos/formulario-novo-curso.php
@@ -0,0 +1,12 @@
+
+<?php include __DIR__.'/../inicio-html.php'; ?>
+
+<form action="">
+    <div class="form-group">
+        <label for="descricao">Descrição</label>
+        <input type="text" name="descricao" placeholder="Descição do curso" class="form-control">
+    </div>
+    <button class="btn btn-primary">Salvar</button>
+</form>
+
+<?php include __DIR__.'/../fim-html.php'; ?>
--- padroesMVC/src/Controller/FormularioInsercao.php
+++ padroesMVC/src/Controller/FormularioInsercao.php
@@ -6,33 +6,7 @@ class FormularioInsercao implements InterfaceControladorRequisicao
 {
     public function processaRequisicao():void
     {
-?>
-        <!DOCTYPE html>
-        <html lang="pt-BR">
-
-        <head>
-            <meta charset="UTF-8">
-            <title>Document</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        </head>
-
-        <body>
-            <div class="container">
-                <div class="jumbotron">
-                    <h1>Novo Curso</h1>
-                </div>
-
-                <form action="">
-                    <div class="form-group">
-                        <label for="descricao">Descrição</label>
-                        <input type="text" name="descricao" placeholder="Descição do curso" class="form-control">
-                    </div>
-                    <button class="btn btn-primary">Salvar</button>
-                </form>
-            </div>
-        </body>
-
-        </html>
-<?php
+        $titulo='Novo Curso';
+        require __DIR__.'/../../View/Cursos/formulario-novo-curso.php';
     }
 }
